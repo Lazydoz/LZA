73aee6a6d24f23937cb38226aa54616b15337cb1
Dell muốn làm <135613676+Lazydoz@users.noreply.github.com>
2025-10-12 03:57:53 +0000

    handldata

---
+++ scripts/handleData.js
@@ -0,0 +1,13 @@
+function handleData(data){
+    const container = document.querySelector('.container');
+    container.innerHTML = '';
+
+    for(let i = 1; i < data.length; i++){
+        const [name, image, link] = data[i];
+        const card = document.createElement('m-card');
+        card.setAttribute('name', name);
+        card.setAttribute('image', image);
+        card.setAttribute('link', link);
+        container.appendChild(card);
+    }
+}
